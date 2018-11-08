46790b996ee07c351a04f82967dd3df0388d59a2
11400523 <11400523@student.pxl.be>
2018-11-08 12:06:40 +0100

    fragment

--- GoSport/app/src/main/java/modev/gosport/View/ListEventsActivity.java
+++ GoSport/app/src/main/java/modev/gosport/View/ListEventsActivity.java
@@ -2,7 +2,10 @@ package modev.gosport.View;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +20,7 @@ public class ListEventsActivity extends FragmentActivity {
         setContentView(R.layout.activity_list_events);
 
         Button makeEvent = (Button) findViewById(R.id.makeEventButton);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         makeEvent.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View view) {
@@ -25,29 +29,30 @@ public class ListEventsActivity extends FragmentActivity {
                     }
                 });
 
-        //DetailFragment detailFragment = (DetailFragment) getFragmentManager().findFragmentById(R.id.detail);
-        //if (detailFragment == null) {
-            /*bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.action_calendar:
-                                Intent intent = new Intent(ListEventsActivity.this, CalendarEventsActivity.class);
-                                startActivity(intent);
-                                break;
-                            case R.id.action_events:
-                                break;
-                            case R.id.action_profile:
-                                intent = new Intent(ListEventsActivity.this, ProfileActivity.class);
-                                startActivity(intent);
-                                break;
-                        }
-                        return false;
-                    }
-                });*/
-        //}
+        DetailFragment detailFragment = (DetailFragment) getFragmentManager().findFragmentById(R.id.detail);
+        if (detailFragment == null || !detailFragment.isVisible()) {
+            bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+            if (bottomNavigationView != null) {
+                bottomNavigationView.setOnNavigationItemSelectedListener(
+                        new BottomNavigationView.OnNavigationItemSelectedListener() {
+                            @Override
+                            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                                switch (item.getItemId()) {
+                                    case R.id.action_calendar:
+                                        Intent intent = new Intent(ListEventsActivity.this, CalendarEventsActivity.class);
+                                        startActivity(intent);
+                                        break;
+                                    case R.id.action_events:
+                                        break;
+                                    case R.id.action_profile:
+                                        intent = new Intent(ListEventsActivity.this, ProfileActivity.class);
+                                        startActivity(intent);
+                                        break;
+                                }
+                                return false;
+                            }
+                        });
+            }
+        }
     }
 }
